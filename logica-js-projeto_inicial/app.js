6f5234b2edf041470d97668ec171f80717da6761
Gabriel Brito <gabriel_brito_300@hotmail.com>
2024-06-24 10:37:57 -0300

    PROJETO TI

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,82 @@
+// alert("ola mundo");
+// let numeroSecreto = 9;
+// let chute = prompt("escolha um numero entre 0 e 10");
+// if(numeroSecreto == chute) {
+//     console.log(`isso ai voce descobriu o codigo ${numeroSecreto}
+//         `) // o que esta colocado nao são aspas é crase
+// }
+// let mensagemDeBoasVindas = 'Boas vindas ao App';
+// alert(mensagemDeBoasVindas);
+// let nome = "lua";
+// alert(nome);
+
+// let dia = prompt("Qual o dia da semana?");
+// if( dia == "sabado" || dia == "domingo") {
+//     alert("bom final de semana")
+// }
+// else {
+//     alert("boa semana")
+// };
+
+// let numero = prompt("digite um numero:")
+// if(numero < 0 ){
+//     alert("numero negativo")
+// }
+// else(
+//     alert("numero positivo")
+// )
+// let pontos = prompt("pontuação do participante:")
+// if (pontos >= 100){
+//     alert("parabens! voce venceu")
+
+// }
+// else (
+//     alert("Tente novamente para ganhar!")
+// )
+
+// // saldo da conta
+// let saldo = 1500
+// console.log(`Seu saldo é  ${saldo}`)
+
+// // boas vindas usuario
+
+// let usuario = prompt("QUAL O SEU NOME?")
+// alert("bem vindo" + "  " + usuario)
+
+
+// // Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+// let cont = 1;
+
+// while( cont <= 10) {
+//     alert( "o contador tem o valor de " + cont );
+//     cont++ ;
+// }
+// //Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+// let num = prompt("Escreva um numero");
+// while( num >= 0) {
+//     alert( "o contador tem o valor de " + num );
+//     num-- ;
+// }
+
+// //Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+// // let lim = prompt("Escreva um numero");
+// // dot = 0;
+// // while( dot <= lim) {
+// //     alert( "o contador tem o valor de " + dot );
+// //     dot++ ; 
+// // }
+
+// mensagem de Boas Vindas
+console.log("Seja Bem Vindo!")
+// Variavel com meu nome e que exibe no console
+let nome = "Gabriel" ;
+console.log( `Bem vindo ${nome}`) ; 
+//Use um loop while para imprimir os números de 1 a 10 no console.
+let cont = 1
+while ( cont <= 10 ) {
+    alert (cont)
+    cont ++
+}
+// Use o Math.randon para gerar qualquer numero aleatorio e exibir no console
+console.log (Math.random()); 
+ console.log (parseInt(Math.random() * 10))
---
+++ logica-js-projeto_inicial/test.js
@@ -0,0 +1,7 @@
+let dia = prompt("Qual o dia da semana?")
+if( dia == "sabado" || "domingo") {
+    alert("bom final de semana")
+}
+else {
+    alert("boa semana")
+}
